fe81945cfbb3a57ad0af300125c9933a615f510c
Mohammad-Abdullah <abdullahofficial2022@gmail.com>
2022-03-26 12:57:39 +0600

    choose 1 showd on ui

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,36 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CartItem from '../CartItem/CartItem';
 import './Cart.css'
 
 const Cart = ({ cartItems, setCart }) => {
-    // console.log(cartItems)
+    const [luckyItem, setLuclyItem] = useState({});
     const itemNumber = Math.floor(Math.random() * cartItems.length);
     const randomItemGenerator = () => {
         if (cartItems.length == 0) {
             return;
         }
-        const luckyItem = cartItems[itemNumber]
-        const { name, img } = luckyItem;
-        console.log(name)
-        const luckyStyle = {
-            display: 'block'
-        }
+        const item = cartItems[itemNumber]
+        setLuclyItem(item)
+        console.log(item)
     }
-    // console.log(randomItem)
     const removeCartItems = () => {
-        setCart([])
+        setCart([cartItems])
     }
-    const luckyStyle = {
-        display: 'none'
-    }
+    const title = <h3 style={{ textAlign: "center" }}>Your lucky Book</h3>
+    const image = <img style={{ width: "150px" }} src={luckyItem.img} alt="" />;
     return (
         <div className='cart'>
             <p className='header-title'>Selected Items</p>
-            <div style={luckyStyle}>hi mama</div>
             {cartItems.map(cartItem => <CartItem
                 key={cartItem.id}
                 cartItem={cartItem}
             ></CartItem>)}
+            <div className='lucky'>
+                {luckyItem.name ? title : ""}
+                {luckyItem.img ? image : ""}
+                <h2> {luckyItem.name}</h2>
+            </div>
             <div>
                 <button onClick={randomItemGenerator} className='cart-btn'><p>CHOOSE 1 FOR ME</p></button>
             </div>
